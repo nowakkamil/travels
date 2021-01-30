65caa26a09fd07aa02b055310260f93bb3ab1c93
nowakkamil <nowakkamil@yahoo.com>
2021-01-30 18:03:51 +0100

    Add a unit test for travels service

--- src/app/services/travels.service.spec.ts
+++ src/app/services/travels.service.spec.ts
@@ -1,16 +1,94 @@
-import { TestBed } from '@angular/core/testing';
+import { inject, TestBed } from '@angular/core/testing';
+import { HttpTestingController, HttpClientTestingModule } from '@angular/common/http/testing';
+import { AngularFireDatabase } from '@angular/fire/database';
+import { of } from 'rxjs';
 
 import { TravelsService } from './travels.service';
+import { Trip } from '../_models/trip';
+
+const tripFixture = {
+  key: 'a',
+  id: 4,
+  description: 'Finest trip description (1)',
+  destination: 'Country #1',
+  name: 'Name: \'1\'',
+  photoUrl: 'https://picsum.photos/id/10/448',
+  startDate: 1608412839805,
+  endDate: 1608412839805,
+  maxPeopleCount: 1,
+  currentPeopleCount: 0,
+  rating: 2,
+  price: 1199,
+  comments: null
+};
+
+const commentsFixture = [{
+  date: 1608412839805,
+  content: 'test content',
+  authorId: 'test authorId',
+  displayTime: 'tes display',
+}];
+
+const tripsFixture: Trip[] = [Trip.fromAngularInterface({ ...tripFixture }, commentsFixture)];
 
 describe('TravelsService', () => {
-  let service: TravelsService;
+  let httpMock: HttpTestingController;
 
-  beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(TravelsService);
+  const snapshotChangesResult = [{
+    payload: {
+      val: () => ({
+        id: 1,
+        key: 'a',
+        description: 'Finest trip description (1)',
+        destination: 'Country #1',
+        name: 'Name: \'1\'',
+        photoUrl: 'https://picsum.photos/id/10/448',
+        startDate: 1608412839805,
+        endDate: 1608412839805,
+        maxPeopleCount: 1,
+        currentPeopleCount: 0,
+        rating: 2,
+        price: 1199,
+        comments: [{
+          date: 1608412839805,
+          content: 'test content',
+          authorId: 'test authorId',
+          displayTime: 'tes display',
+        }]
+      })
+    }
+  }];
+  const listSpy = jasmine.createSpyObj({
+    snapshotChanges: of(snapshotChangesResult)
+  });
+  const angularFireSpy = jasmine.createSpyObj('AngularFireDatabase', {
+    list: listSpy
   });
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [
+        TravelsService,
+        { provide: AngularFireDatabase, useValue: angularFireSpy },
+      ]
+    }).compileComponents();
+
+    httpMock = TestBed.inject(HttpTestingController);
+  });
+
+  it('#getAll', inject([TravelsService], (service: TravelsService) => {
+    const trips$ = service.getAllPromise();
+    trips$.subscribe(trips => {
+      const trip = Trip.fromAngularInterface(trips[0], trips[0].comments);
+      const expectedTrip = tripsFixture[0];
+      expectedTrip.id = Trip.lastId;
+
+      expect(trip).toEqual(expectedTrip);
+    });
+  }));
+
+  afterEach(() => {
+    httpMock.verify();
   });
 });
